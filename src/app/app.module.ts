f3fe6294375db85bdf6426405798608ccd109d86
AnaTorres88 <ana.torresgzzgmail.com>
2025-10-30 00:11:00 -0600

    change typo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
-import { CallPagComponent } from './call-page/call-page.component';
+import { CallPageComponent } from './call-page/call-page.component';
 import { YellPipe } from './features/pipes/yell.pipe';
 
 @NgModule({ 
   declarations: [
     AppComponent,
-    CallPagComponent,
+    CallPageComponent,
     YellPipe
   ],
   imports: [
